a9dba7f636600926a3381e34d34756eb57ae2bfe
mdiqbalahmed <mdiqbaldiu@gmail.com>
2024-05-10 19:02:58 +0600

    comment

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,7 @@ import profile from '../images/profile.png'
 const Header = () => {
     return (
         <div className=' flex justify-between items-center p-4 mx-4 border-b-2'>
-            <h1 className="text-4xl font-bold">Knowledge Cafe</h1>
+            <h1 className="text-4xl font-bold">Knowledge Hub</h1>
             <img src={profile} alt="" />
 
         </div>
